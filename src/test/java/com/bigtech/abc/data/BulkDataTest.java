e99a671fd4cd1a942049636d362c42ea93d39af1
jonghun <jonghuncu@gmail.com>
2023-07-01 22:08:12 +0900

    feat : Member, Like 객체 대량 건 생성 테스트

--- src/test/java/com/bigtech/abc/data/BulkDataTest.java
+++ src/test/java/com/bigtech/abc/data/BulkDataTest.java
@@ -1,5 +1,13 @@
 package com.bigtech.abc.data;
 
+import com.bigtech.abc.exception.AppException;
+import com.bigtech.abc.exception.ErrorCode;
+import com.bigtech.abc.like.Like;
+import com.bigtech.abc.like.JPALikeRepository;
+import com.bigtech.abc.like.LikeService;
+import com.bigtech.abc.member.JPAMemberRepository;
+import com.bigtech.abc.member.Member;
+import com.bigtech.abc.member.MemberService;
 import com.bigtech.abc.post.JPAPostRepository;
 import com.bigtech.abc.post.Post;
 import com.github.javafaker.Faker;
@@ -9,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import org.junit.Assert;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -21,6 +30,18 @@ public class BulkDataTest {
     @Autowired
     private JPAPostRepository jpaPostRepository;
 
+    @Autowired
+    private JPAMemberRepository jpaMemberRepository;
+
+    @Autowired
+    private JPALikeRepository jpaLikeRepository;
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    LikeService likeService;
+
 
     @Test
     @DisplayName("Post객체 100만개 생성")
@@ -31,7 +52,7 @@ public class BulkDataTest {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         LocalDateTime createdDate = LocalDateTime.parse(input, formatter);
 
-        for (int i=0; i < 1000000; i++) {
+        for (int i = 0; i < 1000000; i++) {
             Post post = new Post();
             post.setSubject(faker.address().streetName() + faker.address().streetName() + faker.address().streetName() + faker.address().streetName());
             post.setContent("Poster ip 주소 : " + faker.internet().ipV4Address());
@@ -42,11 +63,12 @@ public class BulkDataTest {
         System.out.println("postList = " + postList.size());
 
         int batchSize = 1000; // 배치 단위 크기
-        int listSize = postList.size();; // 전체 데이터 크기
+        int listSize = postList.size();
+        ; // 전체 데이터 크기
 
         int beforepostCount = jpaPostRepository.getPostCount();
 
-        for (int i=0; i<listSize; i += batchSize) {
+        for (int i = 0; i < listSize; i += batchSize) {
             int endIndex = i + batchSize; // 최소 index를 구한다 save를 할때 batch 단위만큼 subList로 저장하기 위해서
             List<Post> batchList = postList.subList(i, endIndex);
             jpaPostRepository.saveAll(batchList);
@@ -56,9 +78,96 @@ public class BulkDataTest {
         int afterpostCount = jpaPostRepository.getPostCount();
 
         Assert.assertEquals("Post객체 100만개 생성", 1000000, afterpostCount - beforepostCount);
+    }
+
+    @Test
+    @DisplayName("Member객체 100개 생성")
+    void bulkMembers() {
+        List<Member> memberList = new ArrayList<>();
+        Faker faker = new Faker(new Locale("en"));
+        String input = "2022-01-01 11:22:33";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime createdDate = LocalDateTime.parse(input, formatter);
+
+        for (int i = 0; i < 100; i++) {
+            String name = faker.name().name();
+            String email = faker.internet().emailAddress();
+            String password1 = String.valueOf(i + 100);
+
+            Member member = memberService.createMember(name, email, password1, LocalDateTime.now(), createdDate, LocalDateTime.now());
+            memberList.add(member);
+        }
+        System.out.println("memberList = " + memberList.size());
+
+        int batchSize = 10; // 배치 단위 크기
+        int listSize = memberList.size();
+        ; // 전체 데이터 크기
+
+        int beforepostCount = jpaMemberRepository.getMemberCount();
+
+        for (int i = 0; i < listSize; i += batchSize) {
+            int endIndex = i + batchSize; // 최소 index를 구한다 save를 할때 batch 단위만큼 subList로 저장하기 위해서
+            List<Member> batchList = memberList.subList(i, endIndex);
+            jpaMemberRepository.saveAll(batchList);
+            jpaMemberRepository.flush();
+        }
+
+        int afterpostCount = jpaMemberRepository.getMemberCount();
+
+        Assert.assertEquals("Member객체 100만개 생성", 100, afterpostCount - beforepostCount);
+    }
+
+    @Test
+    @DisplayName("이름 email 체크")
+    void name() {
+        Faker faker = new Faker(new Locale("ko"));
+        String name = faker.name().name();
+        String email = faker.internet().emailAddress();
+        System.out.println("email = " + email);
+        System.out.println("name = " + name);
+    }
+    @Test
+    @DisplayName("좋아요 객체 100개 생성")
+    void bulkLikes() {
+        /**
+         * for 루프를 돌며 한명의 회원이 1개의 게시글에 좋아요를 누른다
+         * 즉 like 객체를 만든다
+         * like 객체를 만든 후 like 리스트에 넣는다
+         * 이때 배치를 사용한다
+         * like.create() 에 대해 매번 save를 실행하지 않고 1000번에 대해 list에 저장 후 saveAll을 실행한다
+         */
+        List<Like> likeList = new ArrayList<>();
+
+        Post post = jpaPostRepository.findById(4L)
+                .orElseThrow(() -> new AppException(ErrorCode.INVALID_DATA, "Post가 없습니다"));
+
+        for (int i = 0; i < 100; i++) {
+            Long memberId = (long) (i + 1);
+            Member member = jpaMemberRepository.findById(memberId)
+                    .orElseThrow(() -> new AppException(ErrorCode.INVALID_DATA, "Member가 없습니다"));
 
 
+            Like like = Like.create(member, post);
+            likeList.add(like);
+        }
+        System.out.println("likeList = " + likeList.size());
 
+        int batchSize = 10; // 배치 단위 크기
+        int listSize = likeList.size();
+        ; // 전체 데이터 크기
+
+        int beforepostCount = jpaLikeRepository.getLikeCount();
+
+        for (int i = 0; i < listSize; i += batchSize) {
+            int endIndex = i + batchSize; // 최소 index를 구한다 save를 할때 batch 단위만큼 subList로 저장하기 위해서
+            List<Like> batchList = likeList.subList(i, endIndex);
+            jpaLikeRepository.saveAll(batchList);
+            jpaLikeRepository.flush();
+        }
+
+        int afterpostCount = jpaLikeRepository.getLikeCount();
+
+        Assert.assertEquals("Like객체 100만개 생성", 100, afterpostCount - beforepostCount);
     }
 
 }
