726db7f472e5745b1ebeb648d4812c3e8d248ff2
jonghun <jonghuncu@gmail.com>
2023-07-01 22:07:40 +0900

    learn : 프로세스, 스레드의 생성, 실행 과정 생성 , 실행 시점을 명확하게 알기

--- src/test/java/com/bigtech/abc/data/HelloSync.java
+++ src/test/java/com/bigtech/abc/data/HelloSync.java
@@ -7,10 +7,11 @@ public class HelloSync {
     private static boolean stopRequested;
     public static void main(String[] args) throws InterruptedException {
         StringDisplay sd = new StringDisplay();
-        MyThread[] mts = new MyThread[5];
+        MyThread[] mts = new MyThread[10];
         for (int i=0; i<mts.length; i++) {
             mts[i] = new MyThread(sd, Integer.toString(i));
             mts[i].start();
+            mts[i].join();
+            }
         }
-    }
 }
