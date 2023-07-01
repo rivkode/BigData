ff887d26976c33de06b2b9f95362bcd7e18542ae
jonghun <jonghuncu@gmail.com>
2023-07-01 22:04:53 +0900

    feat : JPAMemberRepository getMemberCount()

--- src/main/java/com/bigtech/abc/member/JPAMemberRepository.java
+++ src/main/java/com/bigtech/abc/member/JPAMemberRepository.java
@@ -1,6 +1,7 @@
 package com.bigtech.abc.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -8,4 +9,7 @@ import java.util.Optional;
 @Repository
 public interface JPAMemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByName(String name);
+
+    @Query("SELECT count(*) FROM Member")
+    Integer getMemberCount();
 }
