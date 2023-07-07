afa2137f9939c62be45b0c14567e81cc19298c57
jonghun <jonghuncu@gmail.com>
2023-07-08 08:13:57 +0900

    feat : like 객체 동시성 테스트

---
+++ src/test/java/com/bigtech/abc/like/LikeTest.java
@@ -0,0 +1,70 @@
+package com.bigtech.abc.like;
+
+import com.bigtech.abc.exception.AppException;
+import com.bigtech.abc.exception.ErrorCode;
+import com.bigtech.abc.member.JPAMemberRepository;
+import com.bigtech.abc.member.Member;
+import com.bigtech.abc.member.MemberService;
+import com.bigtech.abc.post.JPAPostRepository;
+import com.bigtech.abc.post.Post;
+import org.junit.Assert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@SpringBootTest
+public class LikeTest {
+    @Autowired
+    private JPAPostRepository jpaPostRepository;
+
+    @Autowired
+    private JPAMemberRepository jpaMemberRepository;
+
+    @Autowired
+    private JPALikeRepository jpaLikeRepository;
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    LikeService likeService;
+
+    @Test
+    @DisplayName("좋아요 동시성 테스트")
+    void test_like_concurrency() {
+        //given
+
+        int numberOfThreads = 2;
+        ExecutorService service = Executors.newFixedThreadPool(numberOfThreads);
+        CountDownLatch latch = new CountDownLatch(numberOfThreads);
+
+        Post post = jpaPostRepository.findById(7L)
+                .orElseThrow(() -> new AppException(ErrorCode.INVALID_DATA, "Post가 없습니다"));
+
+        Member member1 = jpaMemberRepository.findById(1L)
+                .orElseThrow(() -> new AppException(ErrorCode.INVALID_DATA, "Member가 없습니다"));
+
+        Member member2 = jpaMemberRepository.findById(2L)
+                .orElseThrow(() -> new AppException(ErrorCode.INVALID_DATA, "Member가 없습니다"));
+
+        //when
+        int beforepostCount = jpaLikeRepository.getLikeCount();
+
+        Like like1 = Like.create(member1, post);
+        Like like2 = Like.create(member2, post);
+
+        jpaLikeRepository.save(like1);
+        jpaLikeRepository.save(like2);
+
+        //then
+        int afterpostCount = jpaLikeRepository.getLikeCount();
+
+        Assert.assertEquals("Like객체 1개 생성", 1, afterpostCount - beforepostCount);
+    }
+}
